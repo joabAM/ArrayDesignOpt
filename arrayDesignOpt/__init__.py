0dee7ac59810d405cc64ba64dbada71fcbbcd132
Joab Max Alonso Apaza Martinez <42390850+joabAM@users.noreply.github.com>
2024-01-18 15:37:27 -0500

    Add files via upload

---
+++ arrayDesignOpt/__init__.py
@@ -0,0 +1,1 @@
+from arrayDesignOpt.main import *
---
+++ setup.py
@@ -0,0 +1,17 @@
+
+from setuptools import find_packages, setup
+
+setup(
+    name='arrayDesignOpt',
+    packages=find_packages(include=['arrayDesignOpt', 'resources', 'utils']),
+    version='0.1',
+    description='My first Python library',
+    author='Joab Apaza',
+    # install_requires=[
+    #     'scipy',
+    #     'shapely',
+    #     'numpy==1.19.5',
+    #     'matplotlib==3.6.0',
+    #     'IPython',
+    #     ],
+)
